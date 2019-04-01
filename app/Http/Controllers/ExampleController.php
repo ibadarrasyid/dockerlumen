e074603878e93168966b6d1c876fe3eabda1db6f
ibadarrasyid <ibad.arrasyid@gmail.com>
2019-04-01 21:42:58 +0700

    kodingan
    mantap

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+	public function index()
+    {
+        return "Masuk COk";
+    }
+    //
+}
